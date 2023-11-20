c069e0a7a6e3c9236cb10065b62ba2f3b32be8a9
Alexander <z4t@yandex.ru>
2023-11-20 16:42:45 +0300

    ferst commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+    return <header className="header">
+    <img src="https://klike.net/uploads/posts/2019-09/1568622623_5.jpg"></img>
+  </header>
+}
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Profile = () => {
+    return <div className="content">
+    <div>
+      <img src='https://klike.net/uploads/posts/2018-07/1531908006_1.jpg'></img>
+    </div>
+    <div>
+      ava + description
+    </div>
+    <div>
+      my posts
+      <div>
+        New post
+      </div>
+      <div>
+        post1
+      </div>
+      <div>
+        post2
+      </div>
+    </div>
+  </div>
+}
+
+export default Profile
